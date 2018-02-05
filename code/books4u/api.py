f7778d075ecb4b08caeb3e36476bb77824dc2ab8
1351530910 <chen.he2@mail.mcgill.ca>
2018-02-05 15:36:38 -0500

    backend
    tentative implementation of rating validation;
    not implemented functions listed

--- code/books4u/api.py
+++ code/books4u/api.py
@@ -16,3 +16,30 @@ def get_all_books(request):
         })
 
     return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+def validateRating(Rating):
+    if(Rating<=5&Rating>=1):
+        return true
+    else:
+        return false
+
+def Autenticate(Username,Password):
+    return 0
+
+def getOverallRating():
+    return 0
+
+def getMemberGender(ID):
+    return 0
+
+def getMemberPersonalIntro(ID):
+    return 0
+
+def getMemberStatus(ID):
+    return 0
+
+def getMemberPermission(ID):
+    return 0
+
+
+
