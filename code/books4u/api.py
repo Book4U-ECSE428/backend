3118de683f383a46894ef798453d6f6862e18af2
Michael Ding <michding@users.noreply.github.com>
2018-02-05 16:07:26 -0500

    Create Account api

--- code/books4u/api.py
+++ code/books4u/api.py
@@ -16,3 +16,23 @@ def get_all_books(request):
         })
 
     return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+
+def create_account(request):
+    if request.method == "POST":
+        try:
+            account_name = request.POST.get('name')
+            account_password = request.POST.get('password')
+            account_e_mail = request.POST.get('e_mail')
+            account_gender = request.POST.get('gender')
+        except ValueError:
+            print("Failed getting request")
+        try:
+            new_user = User(name=account_name, password=account_password, e_mail=account_e_mail, gender=account_gender)
+            new_user.save()
+        except(TypeError, ValueError):
+            print("Failed saving data")
+        return HttpResponse("success")
+    else:
+        return HttpResponse("failure")
+
