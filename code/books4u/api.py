9b396d1799745dc85a27d6bc42c1362911e64f20
xiaozhou-liao <xiaozhou.liao@mail.mcgill.ca>
2018-02-06 12:54:01 +0900

    Password encryption
    achieved password encryption by django.contrib.auth

--- code/books4u/api.py
+++ code/books4u/api.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import make_password
+from django.contrib.auth import check_password
 import json
 from .models import *
 
@@ -20,7 +22,7 @@ def create_account(request):
     if request.method == "POST":
         try:
             account_name = request.POST.get('name')
-            account_password = request.POST.get('password')
+            account_password = make_password(request.POST.get('password'))
             account_e_mail = request.POST.get('e_mail')
             account_gender = request.POST.get('gender')
         except ValueError:
@@ -34,26 +36,45 @@ def create_account(request):
     else:
         return HttpResponse(json.dumps({'status': 'failure_method'}))
 
-def validateRating(Rating):
+def validate_rating(Rating):
     if(Rating<=5&Rating>=1):
-        return true
+        return True
     else:
-        return false
+        return False
 
-def Autenticate(Username,Password):
+""" verify the user by checking the existence of email and corresponded password
+    request contains an email and a plain-text password
+"""
+def autentiate(request):
+    if request.method == "POST":
+        try:
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+        except requests.exceptions.RequestException:
+            return HttpResponse(json.dumps({'status': 'missing_required_fields'}))
+
+        #check if the user exists
+        user = User.objects.all()                        
+        for u in User:
+            if u.email == email and check_password(password, u.password):
+                return HttpResponse(json.dumps({'status': 'user authorized'}))
+            
+        return HttpResponse(json.dumps({'status': 'incorrected email or password'}))
+
+    else:
+        return HttpResponse(json.dumps({'status': 'failure_method'}))
+
+def get_overallR_rating():
     return 0
 
-def getOverallRating():
+def get_member_gender(ID):
     return 0
 
-def getMemberGender(ID):
+def get_member_personal_intro(ID):
     return 0
 
-def getMemberPersonalIntro(ID):
+def get_member_status(ID):
     return 0
 
-def getMemberStatus(ID):
-    return 0
-
-def getMemberPermission(ID):
+def get_member_permission(ID):
     return 0
--- code/books4u/tests.py
+++ code/books4u/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.test import Client
+from .api import *
 
 # Create your tests here.
+c = Client()
+
+# Add book
