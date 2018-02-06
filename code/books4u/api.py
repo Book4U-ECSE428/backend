ca47320fe183eb4e91bea21c6285c2b510722913
xiaozhou-liao <xiaozhou.liao@mail.mcgill.ca>
2018-02-06 13:06:10 +0900

    New book from user
    Implemented the mechanism of adding new book - S3T5,S3T6
    User add new book with visibility=false
    The new book become visible once permission is granted, otherwise it is removed.

--- code/books4u/api.py
+++ code/books4u/api.py
@@ -36,6 +36,85 @@ def create_account(request):
     else:
         return HttpResponse(json.dumps({'status': 'failure_method'}))
 
+""" used by users to add a new book
+    visibility of the new book is false, representing not officailly added
+"""
+def add_book(request):
+
+    if request.method == "POST":
+        try:
+            ISBN_ = request.POST.get('ISBN')
+            name_ = request.POST.get('name')
+            publish_date_ = request.POST.get('publish_date','')
+            edition_ = request.POST.get('edition','')
+            category_ = request.POST.get('category')
+            author_ = request.POST.get('author')
+        except requests.exceptions.RequestException:
+            return HttpResponse(json.dumps({'status': 'missing_required_fields'}))
+
+        exist = False
+        #check if the input category exists
+        categories = BookCategory.objects.all()
+        for c in categories:
+            if c.name == category_:
+                exist = True
+                category_o = c
+                break
+        if not exist:
+            category_o = BookCategory(name=category_)
+
+        exist = False
+        #check if the input author exists
+        authors = Author.objects.all()                        
+        for a in Author:
+            if a.name == author_:
+                exist = True
+                author_o = a
+                break
+        if not exist:
+            author_o = Author(name=author_, summary='')
+        
+        new_book = Book(ISBN=ISBN_, name=name_, publish_date=publish_date_, edition=edition_, category_o=category_, author_o=author_)
+        new_book.save()
+    
+    else:
+        return HttpResponse(json.dumps({'status': 'failure_method'}))
+
+""" call this method to check if the moderator should be notified that there is a pending new book
+    return a list of books needed to be verifid
+"""
+def get_pending_books():
+
+    pending_books = []
+    books = Book.objects.all()
+    for book in books:
+        if book.visibility == False:
+            pending_books.append(book)
+
+    return pending_books
+
+""" request contains two values:
+    a bool indicating permmited or not and a str indicating the ISBN of a pending book
+"""
+def commit_book(request):
+
+    if request.method == "POST":
+        try:
+            ISBN_ = request.POST.get('ISBN')
+            permmited = request.POST.get('permit')
+        except requests.exceptions.RequestException:
+            return HttpResponse(json.dumps({'status': 'missing_required_fields'}))
+
+        if permmited:
+            Book.objects.get(ISBN=ISBN_).visibility = True
+            return HttpResponse(json.dumps({'status': 'new book permmited'}))
+        else:
+            Book.objects.get(ISBN=ISBN_).delete()
+            return HttpResponse(json.dumps({'status': 'pending book rejected'}))
+
+    else:
+        return HttpResponse(json.dumps({'status': 'failure_method'}))
+
 def validate_rating(Rating):
     if(Rating<=5&Rating>=1):
         return True
