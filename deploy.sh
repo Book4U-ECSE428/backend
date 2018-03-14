9cb45ecbfd8aa83e6fc70d28d959d30461177d4f
Zisheng Wang <wzs1234566@outlook.com>
2018-03-13 21:49:34 -0400

    sudo docker

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/Book4U-ECSE428/front.git
 mv front/ ./code/ECSE428/static/
 rm -rf front/
-docker build -t books4u-deploy -f Docker-deploy .
-docker swarm init
-docker stack deploy -c docker-compose.yml books4u-deploy
+sudo docker build -t books4u-deploy -f Docker-deploy .
+sudo docker swarm init
+sudo docker stack deploy -c docker-compose.yml books4u-deploy
 # docker swarm leave -f
