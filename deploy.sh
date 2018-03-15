bccc00fbbcb48ee193b2eddbd3f6ed822eb65143
wzs1234566 <wzs1234566@outlook.com>
2018-03-15 00:46:56 -0400

    success redirect

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+sudo docker swarm leave -f
 git clone https://github.com/Book4U-ECSE428/front.git
+mkdir ./code/ECSE428/static/
 mv front/ ./code/ECSE428/static/
-rm -rf front/
 sudo docker build -t books4u-deploy -f Docker-deploy .
 sudo docker swarm init
 sudo docker stack deploy -c docker-compose.yml books4u-deploy
+rm -rf ./code/ECSE428/static/
 # docker swarm leave -f
