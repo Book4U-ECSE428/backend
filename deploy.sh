826f0d467e1bf35c9165a36b3274f379b74df788
wzs1234566 <wzs1234566@outlook.com>
2018-03-28 21:58:40 -0400

    cahnge to a cheap db

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ git clone https://github.com/Book4U-ECSE428/front.git
 mkdir ./code/ECSE428/static/
 mv front/ ./code/ECSE428/static/
 sudo docker build -t books4u-deploy -f Docker-deploy .
-sudo docker swarm init
-sudo docker stack deploy -c docker-compose.yml books4u-deploy
+sudo docker run -d -p 80:80 -p 8080:8080 -p 8000:8000 books4u-deploy
+#sudo docker swarm init
+#sudo docker stack deploy -c docker-compose.yml books4u-deploy
 rm -rf ./code/ECSE428/static/
